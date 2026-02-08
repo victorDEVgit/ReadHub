86a53eab01c1f1be4f395069a95d951bf473493c
Victor <victoragboighale@gmail.com>
2026-02-08 11:13:56 +0100

    Removed duplicate ReadhubFrontend folder

--- ReadhubFrontend/src/Pages/Auth/validate.js
+++
@@ -1,7 +0,0 @@
-export const validateEmail = (email) => {
-    if(email.trim()){
-        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return regex.test(email);
-    }
-    return false;
-}
